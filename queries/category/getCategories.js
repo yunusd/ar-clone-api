da41a73837861e6a8d3d82afdfad4e48638724dd
emre.cansiz <emre.cansiz@bilgeadam.com>
2021-01-09 17:33:21 +0300

    getcategories fix

--- queries/category/getCategories.js
+++ queries/category/getCategories.js
@@ -3,27 +3,28 @@ const lodash = require('lodash');
 module.exports = async (...args) => {
   const [, params, context, ] = args;
   let categories = await context.models.Category.findAll({
-    include: {
-      model: context.models.Service,
-      as: "services"
-    },
-    include: {
-      model: context.models.Faq,
-      as: "faqs"
-    },
-    include: {
-      model: context.models.RequiredDocument,
-      foreingkey: "categoryId" ,
-      as: "requiredDocuments"
-    },
-    include: {
-      model: context.models.Question,
-      as: "questions",
-      include: {
-        model: context.models.QuestionOption,
-        as: "options"
-      }
-    },
+    include: [{
+        model: context.models.Service,
+        as: "services"
+      },
+      {
+        model: context.models.Faq,
+        as: "faqs"
+      },
+      {
+        model: context.models.RequiredDocument,
+        foreingkey: "categoryId",
+        as: "requiredDocuments"
+      },
+      {
+        model: context.models.Question,
+        as: "questions",
+        include: {
+          model: context.models.QuestionOption,
+          as: "options"
+        }
+      },
+    ]
   });
 
   if (params.catalogId != null && categories != null) {
